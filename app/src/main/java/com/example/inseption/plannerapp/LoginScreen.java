4a591335e02ed1623794d83d74b78d51b5cdefae
up819025 <35937648+up819025@users.noreply.github.com>
2018-03-10 10:51:56 +0000

    V1.1
    Variable names and values modified. Sign out functionality added.

--- app/src/main/java/com/example/inseption/plannerapp/LoginScreen.java
+++ app/src/main/java/com/example/inseption/plannerapp/LoginScreen.java
@@ -3,6 +3,7 @@ package com.example.inseption.plannerapp;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -82,7 +83,7 @@ public class LoginScreen extends AppCompatActivity implements LoaderCallbacks<Cu
             }
         });
 
-        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+        Button mEmailSignInButton = (Button) findViewById(R.id.sign_in_button);
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -159,8 +160,12 @@ public class LoginScreen extends AppCompatActivity implements LoaderCallbacks<Cu
         boolean cancel = false;
         View focusView = null;
 
-        // Check for a valid password, if the user entered one.
-        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+        // Check for a valid password.
+        if (TextUtils.isEmpty(password)) {
+            mPasswordView.setError(getString(R.string.error_field_required));
+            focusView = mPasswordView;
+            cancel = true;
+        } else if (!isPasswordValid(password)) {
             mPasswordView.setError(getString(R.string.error_invalid_password));
             focusView = mPasswordView;
             cancel = true;
@@ -187,17 +192,19 @@ public class LoginScreen extends AppCompatActivity implements LoaderCallbacks<Cu
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
+            startActivity(new Intent(LoginScreen.this, Calendar.class));
+
         }
     }
 
-    private boolean isEmailValid(String email) {
+    private boolean isEmailValid(String username) {
         //TODO: Replace this with your own logic
-        return email.contentEquals("up819025");
+        return username.contentEquals("username");
     }
 
     private boolean isPasswordValid(String password) {
         //TODO: Replace this with your own logic
-        return password.contentEquals("ss");
+        return password.contentEquals("password");
     }
 
     /**
@@ -345,6 +352,8 @@ public class LoginScreen extends AppCompatActivity implements LoaderCallbacks<Cu
             mAuthTask = null;
             showProgress(false);
         }
+
+
     }
 }
 
